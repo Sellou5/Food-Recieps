63630dd23a2c06951e43aa21760a3293c0de8722
Ahmed Sellou <35193952+Sellou5@users.noreply.github.com>
2019-05-02 11:22:15 +0200

    Add files via upload

---
+++ Foood.Shared/InternetConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Networking.Connectivity;
+
+namespace Foood
+{
+    class InternetConnection
+    {
+        public static bool IsConnectedToInternet()
+        {
+            ConnectionProfile connectionProfile = NetworkInformation.GetInternetConnectionProfile();
+            return (connectionProfile != null && connectionProfile.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess);
+        }
+    }
+}
